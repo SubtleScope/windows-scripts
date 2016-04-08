12b162b089774f620d055082339fd08d600fead3
SubtleScope <wray.nathan@gmail.com>
2016-04-08 18:29:19 -0400

    Updating Modify Dates Module

--- modify_dates.ps1
+++ modify_dates.ps1
@@ -1,23 +1,57 @@
-# Written by: Nathan R. Wray (m4dh4tt3r)
-#
-## About: Navigate to the directory where you want modify the 
-##        Access, Creation, and Midufued Timestamps
-##        This script recursively sets the timestamps to a time
-##        within the given min/max date range
-##
-## Usage: > cd C:\Users\jimbob\artifacts\
-##        > modify_dates.ps1
-##
-## Todo: Add in the capability to specify a directory or file(s)
-##       rather than just recursively modify everything
-##       Enhance the script to provide more functionality
+function Invoke-ModifyDates {
+  <#
+    .NAME
+    Invoke-ModifyDates
 
-# Set the Date Time Range
-$dateMin = (Get-Date -year 2014 -month 1 -day 9)
-$dateMax = (Get-Date -year 2015 -month 7 -day 31)
+    .SYNOPSIS
+    Module is used to modify the LastAccess and CreationTime of files in a specified directory given a specific date range.
 
-# Modify the Access Time Recursively within a Directory
-gci -rec | %{$_.lastWriteTime = ($_.lastAccessTime = (new-object datetime (Get-Random -min $dateMin.ticks -max $dateMax.ticks))) }
+    .AUTHOR
+    Nathan Wray (m4dh4tt3r)
 
-# Modify the Creation Time Recursively within a Directory
-gci -rec | %{$_.lastWriteTime = ($_.creationTime = (new-object datetime (Get-Random -min $dateMin.ticks -max $dateMax.ticks))) }
+    .USAGE
+    C:\> Invoke-ModifyDates -Path [C:\Path\To\Directory] -MinDate [MM.DD.YYYY] -MaxDate [MM.DD.YYYY]
+
+    .EXAMPLE
+    C:\> Import-Module Invoke-ModifyDates.ps1
+    C:\> Invoke-ModifyDates -Path C:\Users\sysroot\Desktop -MinDate '01.01.2000' -MaxDate '01.30.2000'
+
+    .BUGS
+    1) As it is currently written, it is possible to have files with a CreationTime later than the LastAccessTime/LastWriteTime date
+
+      PS C:\> (gci Get-Test.ps1).CreationTime
+
+      Friday, January 28, 2000 5:20:12 PM
+
+      PS C:\> (gci Get-Finger.ps1).LastAccessTime
+
+      Monday, January 17, 2000 1:36:23 AM
+  #>
+
+  Param(
+    [Parameter(Position = 0,
+               Mandatory = $TRUE)]
+    [String]$Path,
+    [String]$MinDate,
+    [String]$MaxDate
+  )
+
+  $tempMinDate = [DateTime]::Parse($MinDate)
+  $tempMaxDate = [DateTime]::Parse($MaxDate)
+  $dateMin = (Get-Date -year ($tempMinDate).Year -month ($tempMinDate).Month -day ($tempMinDate).Day)
+  $dateMax = (Get-Date -year ($tempMaxDate).Year -month ($tempMaxDate).Month -day ($tempMaxDate).Day)
+
+  # Modify Creation Time
+  #
+  # Shothand Version
+  # gci -Path $Path -rec | % { $_.creationTime = (new-object datetime (Get-Random -min $dateMin.ticks -max $dateMax.ticks)) }
+  #
+  Get-ChildItem -Path $Path -Recurse | ForEach-Object { $_.creationTime = (New-Object DateTime (Get-Random -Minimum $dateMin.ticks -Maximum $dateMax.ticks)) }
+
+  # Modify Access Time
+  #
+  # Shorthand Version
+  # gci -Path $Path -rec | % { $_.lastWriteTime = ($_.lastAccessTime = (new-object datetime (get-random -min $dateMin.ticks -max $dateMax.ticks))) }
+  # 
+  Get-ChildItem -Path $Path -Recurse | ForEach-Object { $_.lastWriteTime = ($_.lastAccessTime = (New-Object DateTime (Get-Random -Minimum $dateMin.ticks -Maximum $dateMax.ticks))) }
+}
